a4820608222ac2a08ee35df44a2ee786194e9b5d
veerakumarcse24 <veerakumarcse24@gmail.com>
2020-05-31 16:00:58 +0530

    first commit

---
+++ src/main/java/com/veerakumarcse24/recipezoon/RecipezoonApplication.java
@@ -0,0 +1,13 @@
+package com.veerakumarcse24.recipezoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipezoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipezoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veerakumarcse24/recipezoon/RecipezoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veerakumarcse24.recipezoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipezoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
