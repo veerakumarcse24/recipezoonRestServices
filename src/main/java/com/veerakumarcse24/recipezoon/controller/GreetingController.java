d64aec88fa37333648e1b46ff5478ad75bc3e5c4
veerakumarcse24 <veerakumarcse24@gmail.com>
2020-05-31 21:38:06 +0530

    cs12019: configurations base setup

--- src/main/java/com/veerakumarcse24/recipezoon/controller/GreetingController.java
+++ src/main/java/com/veerakumarcse24/recipezoon/controller/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import com.veerakumarcse24.recipezoon.modal.Greeting;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    @Value("${app.name}")
+    private String appName;
+
+    private static final String template = "Hello %s, welcome to the %s";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting(counter.incrementAndGet(), String.format(template, name, appName));
 	}
 
 }
